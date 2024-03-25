d6f3048d3851566111423e6e75d6a6c85ab78090
Kendall Madrigal <kendallmadrigal14@gmail.com>
2024-03-25 11:44:48 -0600

    Servicio de cultivos para la rotacion de cultivos

--- src/servicios/ServicioCultivo.ts
+++ src/servicios/ServicioCultivo.ts
@@ -5,19 +5,38 @@ export const ObtenerCalidadCultivos = async () => {
     const url = `http://localhost:5271/api/v1.0/${controlador}/ObtenerCalidadCultivos`;
     return await ProcesarDatosApi('GET', url, '');
 }
+export const ObtenerRotacionCultivoSegunEstacionalidad = async () => {
+    const url = `http://localhost:5271/api/v1.0/${controlador}/ObtenerRotacionCultivoSegunEstacionalidad`;
+    return await ProcesarDatosApi('GET', url, '');
+}
+
+export const InsertarRotacionCultivoSegunEstacionalidad = async (data: any) => {
+    const url = `http://localhost:5271/api/v1.0/${controlador}/InsertarRotacionCultivoSegunEstacionalidad`;
+    return await ProcesarDatosApi('POST', url, data);
+}
 
 export const EditarCalidadCultivo = async (data: any) => {
     const url = `http://localhost:5271/api/v1.0/${controlador}/EditarCalidadCultivo`;
     return await ProcesarDatosApi('PUT', url, data);
 }
-  
+export const ModificarRotacionCultivoSegunEstacionalidad = async (data: any) => {
+    const url = `http://localhost:5271/api/v1.0/${controlador}/ModificarRotacionCultivoSegunEstacionalidad`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
+
+
 export const AgregarCalidadCultivo = async (data: any) => {
     const url = `http://localhost:5271/api/v1.0/${controlador}/AgregarCalidadCultivo`;
     return await ProcesarDatosApi('POST', url, data);
 }
- 
+
 export const CambiarEstadoCalidadCultivo = async (data: any) => {
     const url = `http://localhost:5271/api/v1.0/${controlador}/CambiarEstadoCalidadCultivo`;
     return await ProcesarDatosApi('PUT', url, data);
 }
 
+
+export const CambiarEstadoRotacionCultivo = async (data: any) => {
+    const url = `http://localhost:5271/api/v1.0/${controlador}/CambiarEstadoRotacionCultivo`;
+    return await ProcesarDatosApi('PUT', url, data);
+}
