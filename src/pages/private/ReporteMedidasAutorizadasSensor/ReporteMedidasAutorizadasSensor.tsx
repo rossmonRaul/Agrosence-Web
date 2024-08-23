47eb7a47ff7714a7bd4d98978a698099a901a755
Carlos Carvajal <carlos.carvajal@rossmon.co.cr>
2024-08-23 10:41:17 -0600

    Reporte PDF Medidas Autorizadas Sensor
    Se agrega opción para generar el reporte PDF de Medidas Autorizadas por Sensor

--- src/pages/private/ReporteMedidasAutorizadasSensor/ReporteMedidasAutorizadasSensor.tsx
+++ src/pages/private/ReporteMedidasAutorizadasSensor/ReporteMedidasAutorizadasSensor.tsx
@@ -7,11 +7,14 @@ import Topbar from "../../../components/topbar/Topbar.tsx";
 import { ObtenerReporteMedidasAutorizadasSensor } from "../../../servicios/ServicioReporte.ts";
 import { IoFilter } from "react-icons/io5";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileExcel } from "@fortawesome/free-solid-svg-icons";
 import { exportToExcel } from '../../../utilities/exportReportToExcel.ts';
 import Swal from 'sweetalert2';
 import { ObtenerFincas } from "../../../servicios/ServicioFincas.ts";
 import { ObtenerParcelas } from '../../../servicios/ServicioParcelas';
+import { faFileExcel, faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import logo from '../../../img/AGROSENSER.png';
 
 function formatFecha(pFecha: string) {
     const fecha = new Date(pFecha);
@@ -185,6 +188,94 @@ function ReporteMedidasAutorizadasSensor() {
         }
     };
 
+    const convertirImagenABase64 = (ruta:any):Promise<string> => {
+        return new Promise((resolve) => {
+            const xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                const reader = new FileReader();
+                reader.onloadend = function () {
+                    resolve(reader.result as string);
+                };
+                reader.readAsDataURL(xhr.response);
+            };
+            xhr.open('GET', ruta);
+            xhr.responseType = 'blob';
+            xhr.send();
+        });
+    };
+
+    // Función para obtener la fecha y hora formateadas
+    const getFormattedDateTime = () => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = String(now.getMonth() + 1).padStart(2, "0");
+        const day = String(now.getDate()).padStart(2, "0");
+        const hours = String(now.getHours()).padStart(2, "0");
+        const minutes = String(now.getMinutes()).padStart(2, "0");
+        const seconds = String(now.getSeconds()).padStart(2, "0");
+
+        return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
+    };
+
+    const generatePDF = async () => {
+        const doc = new jsPDF();
+    
+        doc.setFontSize(18);
+        doc.setTextColor(0, 0, 0);
+        doc.text("Mediciones Autorizadas de Sensor", 60, 10);
+    
+        const imgBase64 = await convertirImagenABase64(logo);
+        const imgWidth = 30;
+        const imgHeight = 30;
+    
+        doc.addImage(imgBase64, 'PNG', 170, -5, imgWidth, imgHeight);
+    
+        doc.setFontSize(10);
+        doc.setTextColor(0, 0, 0);
+        doc.text("Usuario: " + userName, 14, 25);
+    
+        doc.setFontSize(10);
+        doc.setTextColor(0, 0, 0);
+        doc.text("Fecha: " + getFormattedDateTime().slice(0, -9), 165, 25);
+    
+        autoTable(doc, {
+          startY: 30,
+          columns: [
+            { key: 'euiSensor', header: 'EUI Sensor' },
+            { key: 'nombreSensor', header: 'Nombre de Sensor' },
+            { key: 'puntoMedicion', header: 'Punto de Medición' },
+            { key: 'parcela', header: 'Parcela' },
+            { key: 'finca', header: 'Finca' },
+            { key: 'idMedicion', header: 'ID Medición' },
+            { key: 'nombreMedicion', header: 'Nombre Medición' },
+            { key: 'unidadMedida', header: 'Unidad Medida' },
+            { key: 'nomenclatura', header: 'Nomenclatura' },
+            { key: 'valor', header: 'Valor' },
+            { key: 'fecha', header: 'Fecha' },
+            { key: 'alerta', header: 'Alerta' }
+          ],
+          headStyles: { fillColor: [84, 132, 84], halign: 'center', valign: 'middle' },
+          body: apiData,
+          columnStyles: {
+            euiSensor: { cellWidth: 'auto' },
+            nombreSensor: { cellWidth: 'auto' },
+            puntoMedicion: { cellWidth: 'auto' },
+            parcela: { cellWidth: 'auto' },
+            finca: { cellWidth: 'auto' },
+            idMedicion: { cellWidth: 'auto' },
+            nombreMedicion: { cellWidth: 'auto' },
+            unidadMedida: { cellWidth: 'auto' },
+            nomenclatura: { cellWidth: 'auto' },
+            valor: { cellWidth: 'auto' },
+            fecha: { cellWidth: 'auto' },
+            alerta: { cellWidth: 'auto' }
+          },
+          styles: {fontSize: 5, halign: 'center', valign: 'middle'}
+        });
+    
+        doc.save("MedidasAutorizadasSensor.pdf");
+      };    
+
     // Columnas de la tabla
     const columns = [
         { key: 'euiSensor', header: 'EUI Sensor', width: 15 },
@@ -253,13 +344,22 @@ function ReporteMedidasAutorizadasSensor() {
                             <IoFilter size={27} />
                             <span style={{ marginLeft: '5px' }}>Filtrar</span>
                         </button>
-                        {apiData.length > 0 &&
-                            <button onClick={() => exportToExcel({ reportName, data: apiData, columns, userName })}  className="btn-exportar" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        {apiData.length > 0 && (
+                            <>
+                                <button onClick={() => generatePDF()} className="btn-exportar" style={{ display: "flex", justifyContent: "center", alignItems: "center",}}>
+                                
+                                <FontAwesomeIcon icon={faFilePdf} style={{ color: "#0CF25D", fontSize: "27px" }} />
+                                <span style={{ marginLeft: "5px" }}>Imprimir</span>
+                                </button>
+                                
+                                <button onClick={() => exportToExcel({ reportName, data: apiData, columns, userName })}  className="btn-exportar" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
 
-                            <FontAwesomeIcon icon={faFileExcel} style={{ color: "#0CF25D", fontSize: '27px' }} />                                
-                            <span style={{ marginLeft: '5px' }}>Exportar</span>
+                                <FontAwesomeIcon icon={faFileExcel} style={{ color: "#0CF25D", fontSize: '27px' }} />                                
+                                <span style={{ marginLeft: '5px' }}>Exportar</span>
 
-                            </button>
+                                </button>
+                            </>
+                        )                        
                         }                        
                     </div>
                     <br />
